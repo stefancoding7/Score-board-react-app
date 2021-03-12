a8e5c3bd2524000ac2259b39aaf1e76bfb78dfe0
Stefan Caky <stefan.caky777@gmail.com>
2021-03-12 20:14:58 +0000

    Counter and Player component
    Created Player and counter component and added in App component than rendered

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function Header() {
+const Header = () => {
   return (
     <header>
       <h1>Scoreboard</h1>
@@ -8,7 +8,41 @@ function Header() {
 };
 
 
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Stefan
+      </span>
+
+      <Counter />
+      
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* Player list*/}
+      <Player />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <Header />,
+  <App />,
   document.getElementById('root')
 )
